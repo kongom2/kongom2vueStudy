e87dec52f1e137e1601c0001eac9d63d025dba7d
kongom2@gmail.com <kongom2@gmail.com>
2022-03-10 17:08:21 +0900

    05 가위바위보

---
+++ 05 가위바위보/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import RockScissorsPaper from "./RockScissorsPaper";
+
+const app = createApp(RockScissorsPaper);
+
+app.mount("#root");
---
+++ 05 가위바위보/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  mode: "development", // 배포시 'production'
+  devtool: "eval", // 배포시 'hidden-source-map' => import 해올 때 확장자를 안붙여도 됨
+  resolve: {
+    extensions: [".js", ".vue"], // 확장자 처리
+  },
+  entry: {
+    app: path.join(__dirname, "main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
+};
