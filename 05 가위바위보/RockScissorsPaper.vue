3cfde38abeb93f14366f2d2c58898e945568f6f7
kongom2@gmail.com <kongom2@gmail.com>
2022-03-11 00:14:49 +0900

    05 가위바위보 : vue 라이프사이클

--- 05 가위바위보/RockScissorsPaper.vue
+++ 05 가위바위보/RockScissorsPaper.vue
@@ -17,6 +17,21 @@ const rspCoords = {
   가위: "-142px",
   보: "-284px",
 };
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function (v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+let interval = null;
+
 export default {
   data() {
     return {
@@ -33,7 +48,58 @@ export default {
     },
   },
   methods: {
-    onClickButton(choice) {},
+    changeHand() {
+      interval = setInterval(() => {
+        if (this.imgCoord === rspCoords.바위) {
+          this.imgCoord = rspCoords.가위;
+        } else if (this.imgCoord === rspCoords.가위) {
+          this.imgCoord = rspCoords.보;
+        } else if (this.imgCoord === rspCoords.보) {
+          this.imgCoord = rspCoords.바위;
+        }
+      }, 100);
+    },
+    onClickButton(choice) {
+      clearInterval(interval);
+      const myScore = scores[choice];
+      const cpuScore = scores[computerChoice(this.imgCoord)];
+      const diff = myScore - cpuScore;
+      if (diff === 0) {
+        this.result = "비겼습니다.";
+      } else if ([-1, 2].includes(diff)) {
+        this.result = "이겼습니다.";
+        this.score += 1;
+      } else {
+        this.result = "졌습니다.";
+        this.score -= 1;
+      }
+      setTimeout(() => {
+        this.changeHand();
+      }, 1000);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate-check");
+  },
+  created() {
+    console.log("created-check");
+  },
+  beforeMount() {
+    console.log("beforeMount-check");
+  },
+  mounted() {
+    console.log("mounted-check");
+    this.changeHand();
+  },
+  updated() {
+    console.log("updated-check");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+    clearInterval(interval);
+  },
+  destroyed() {
+    console.log("destroyed-check");
   },
 };
 </script>
