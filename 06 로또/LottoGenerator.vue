fb481b3ce436cdeb2dd1415e62c03074e66ba05e
kongom2@gmail.com <kongom2@gmail.com>
2022-03-11 15:18:11 +0900

    06 로또 : 추첨기 구현

--- 06 로또/LottoGenerator.vue
+++ 06 로또/LottoGenerator.vue
@@ -2,27 +2,80 @@
   <div>
     <div>당첨숫자</div>
     <div id="결과창">
-      <lotto-ball v-for="ball in winBalls" :key="ball"></lotto-ball>
+      <lotto-ball
+        v-for="ball in winBalls"
+        :key="ball"
+        :number="ball"
+      ></lotto-ball>
     </div>
     <div>보너스</div>
-    <lotto-ball v-if="bonus"></lotto-ball>
-    <button v-if="redo">한 번 더!</button>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if="redo" @click="onClickRedo">한 번 더!</button>
   </div>
 </template>
 
 <script>
+import LottoBall from "./LottoBall";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+const timeouts = [];
+
 export default {
+  components: {
+    "lotto-ball": LottoBall,
+  },
   data() {
     return {
+      winNumbers: getWinNumbers(),
       winBalls: [],
       bonus: null,
       redo: false,
     };
   },
   computed: {},
-  methods: {},
-  mounted() {},
-  beforeDestroy() {},
+  methods: {
+    showBalls() {
+      for (let i = 0; i < this.winNumbers.length - 1; i++) {
+        timeouts[i] = setTimeout(() => {
+          this.winBalls.push(this.winNumbers[i]);
+        }, (i + 1) * 1000);
+      }
+      timeouts[6] = setTimeout(() => {
+        this.bonus = this.winNumbers[6];
+        this.redo = true;
+      }, 7000);
+    },
+    onClickRedo() {
+      this.winNumbers = getWinNumbers();
+      this.winBalls = [];
+      this.bonus = null;
+      this.redo = false;
+      this.showBalls();
+    },
+  },
+  mounted() {
+    this.showBalls();
+  },
+  beforeDestroy() {
+    timeouts.forEach((t) => {
+      clearTimeout(t);
+    });
+  },
   watch: {},
 };
 </script>
